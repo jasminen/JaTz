d2ccfb2270413e7387b1a847ad86148990bfab92
jasminen <jasminen@liveperson.com>
2014-04-26 20:03:08 +0300

    Finally fixing merge issues

--- JaTz/src/model/game2048/MoveRight2048Action.java
+++ JaTz/src/model/game2048/MoveRight2048Action.java
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
+
 package model.game2048;
+
 
 import model.Action;
 import model.State;
@@ -46,52 +47,4 @@ public class MoveRight2048Action implements Action {
 	}
 
 }
-=======
-package model.game2048;
 
-import model.Action;
-import model.State;
-
-public class MoveRight2048Action implements Action {
-
-	@Override
-	public State doAction(State state) {
-		int[][] newBoard=state.getBoard();
-		int newScore=state.getScore();
-		
-		for (int i = 0; i < newBoard.length ; i++) {
-			Boolean sumFlag=true;
-			
-			for (int j = newBoard[0].length-2; j >=0 ; j--) {
-				
-				if(newBoard[i][j] ==0)
-					continue;
-				
-				for (int h = j; h<newBoard[0].length-1 ; h++) {
-					
-					if(newBoard[i][h+1] == 0 ) {
-						newBoard[i][h+1]=newBoard[i][h];
-						newBoard[i][h]=0;
-						continue;
-					}
-						
-					else if(newBoard[i][h+1] == newBoard[i][h] && sumFlag) {
-						newBoard[i][h+1]+=newBoard[i][h];
-						newBoard[i][h]=0;
-						sumFlag=false;	
-						newScore+=newBoard[i][h+1];
-						break;
-					}
-					else {
-						sumFlag=true;
-						break;
-					}			
-				}
-			}
-		}
-		
-		return new State(newBoard, newScore, state.getMode());
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/jasminen/JaTz.git
--- JaTz/src/model/game2048/MoveUp2048Action.java
+++ JaTz/src/model/game2048/MoveUp2048Action.java
@@ -1,5 +1,6 @@
-<<<<<<< HEAD
+
 package model.game2048;
+
 
 import model.Action;
 import model.State;
@@ -46,52 +47,4 @@ public class MoveUp2048Action implements Action {
 	}
 
 }
-=======
-package model.game2048;
 
-import model.Action;
-import model.State;
-
-public class MoveUp2048Action implements Action {
-
-	@Override
-	public State doAction(State state) {
-		int[][] newBoard=state.getBoard();
-		int newScore=state.getScore();
-		
-		for (int i = 0; i < newBoard[0].length ; i++) {
-			Boolean sumFlag=true;
-			
-			for (int j = 1; j < newBoard.length; j++) {
-				
-				if(newBoard[j][i] ==0)
-					continue;
-				
-				for (int h = j; h>0 ; h--) {
-					
-					if(newBoard[h-1][i] == 0 ) {
-						newBoard[h-1][i]=newBoard[h][i];
-						newBoard[h][i]=0;
-						continue;
-					}
-						
-					else if(newBoard[h-1][i] == newBoard[h][i] && sumFlag) {
-						newBoard[h-1][i]+=newBoard[h][i];
-						newBoard[h][i]=0;
-						sumFlag=false;	
-						newScore+=newBoard[h-1][i];
-						break;
-					}
-					else {
-						sumFlag=true;
-						break;
-					}			
-				}
-			}
-		}
-		
-		return new State(newBoard, newScore, state.getMode());
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/jasminen/JaTz.git
